92915e027cc921266a9a9d6b6c7d94aabec29bfc
DipNarayanMahara2 <dipnarayanmahara28@gmail.com>
2025-06-02 22:30:14 +0545

    .

--- middleware/restricTo.js
+++ middleware/restricTo.js
@@ -1,10 +1,6 @@
-
-
-
 const restricTo = (...roles) => {
   return (req, res, next) => {
     const userRole = req.user.role;
-    console.log(userRole);
     if (!roles.includes(userRole)) {
       res.status(403).json({
         message: "Access Denied. Admin only",
